bd27d4fc541aba484ed462ab92a9079c837aa5cb
Allen Njiva <anjiva7@gmail.com>
2023-04-25 00:56:01 -0700

    Printf

--- main.c
+++ main.c
@@ -3,42 +3,10 @@
 #include "main.h"
 
 /**
- * main - Entry point
- *
- * Return: Always 0
- */
-int main(void)
-{
-int len;
-int len2;
-unsigned int ui;
-void *addr;
 
-len = _printf("Let's try to printf a simple sentence.\n");
-len2 = printf("Let's try to printf a simple sentence.\n");
-ui = (unsigned int)INT_MAX + 1024;
-addr = (void *)0x7ffe637541f0;
-_printf("Length:[%d, %i]\n", len, len);
-printf("Length:[%d, %i]\n", len2, len2);
-_printf("Negative:[%d]\n", -762534);
-printf("Negative:[%d]\n", -762534);
-_printf("Unsigned:[%u]\n", ui);
-printf("Unsigned:[%u]\n", ui);
-_printf("Unsigned octal:[%o]\n", ui);
-printf("Unsigned octal:[%o]\n", ui);
-_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
-_printf("Character:[%c]\n", 'H');
-printf("Character:[%c]\n", 'H');
-_printf("String:[%s]\n", "I am a string !");
-printf("String:[%s]\n", "I am a string !");
-_printf("Address:[%p]\n", addr);
-printf("Address:[%p]\n", addr);
-len = _printf("Percent:[%%]\n");
-len2 = printf("Percent:[%%]\n");
-_printf("Len:[%d]\n", len);
-printf("Len:[%d]\n", len2);
-_printf("Unknown:[%r]\n");
-printf("Unknown:[%r]\n");
-return (0);
+_printf - function that produces output according to a format.
+@format: a string with format specifiers
+Return: the number of characters printed.
+*/
+int _printf(const char *format, ...) { va_list args; int i = 0, count = 0, formatCount = 0; char str; char string[1024]; if (format == NULL) return (-1); va_start(args, format); while (format[i] != '\0') { if (format[i] == '%') { formatCount = 1; switch (format[++i]) { case 'c': putchar(va_arg(args, int)); count++; break; case 's': str = va_arg(args, char); while (*str != '\0') { putchar(*str); count++; str++; } break; case 'd': case 'i': printf("%d", va_arg(args, int)); break; case '%': putchar('%'); count++; break; default: putchar('%'); putchar(format[i]); count += 2; break; } } else { putchar(format[i]); count++; } i++; } va_end(args); return (count);
 }
